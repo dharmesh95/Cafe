f7104fc0a82ca078dd0129990a3b4f668fb7afc7
dharmesh95 <95dharmesh@gmail.com>
2019-02-08 03:10:52 -0500

    First commit

---
+++ src/main/java/com/timhortons/brew/TimHortonsApplication.java
@@ -0,0 +1,14 @@
+package com.timhortons.brew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimHortonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimHortonsApplication.class, args);
+	}
+
+}
+
